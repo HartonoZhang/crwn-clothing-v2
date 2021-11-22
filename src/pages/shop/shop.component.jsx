eb450a154fa7cfe2b2451f52c138bbe636313066
HartonoZhang <hartonobestzx@gmail.com>
2021-11-22 21:03:02 +0700

    adding route collection per category

--- src/pages/shop/shop.component.jsx
+++ src/pages/shop/shop.component.jsx
@@ -1,30 +1,13 @@
-import { Component } from "react";
+import { Route } from "react-router-dom";
 
-import SHOP_DATA from "./shop.data";
+import CollectionOverview from "../../components/collection-overview/collection-overview.component";
+import CollectionPage from "../collection/collection.component";
 
-import CollectionPreview from "../../components/collection-preview/collection-preview.component";
-
-class ShopePage extends Component{
-  constructor(props){
-    super(props);
-  
-    this.state = {
-      collections: SHOP_DATA
-    }
-  }
-
-  render(){
-    const { collections } = this.state;
-    return (
-      <div className='shop-page'>
-        {
-          collections.map(({ id, ...otherCollectionProps}) => (
-            <CollectionPreview key={id} {...otherCollectionProps} />
-          ))
-        }
-      </div>
-    )
-  }
-}
+const ShopePage = ({ match }) => (
+  <div className="shop-page">
+      <Route exact path={`${match.path}`} component={CollectionOverview} />
+      <Route path={`${match.path}/:collectionId`} component={CollectionPage} />
+  </div>
+);
 
 export default ShopePage;
