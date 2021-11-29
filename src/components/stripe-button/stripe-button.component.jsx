c051aac07919a4ea203f1ce3f560e73973fa25b7
HartonoZhang <hartonobestzx@gmail.com>
2021-11-29 21:56:46 +0700

    updating stripe integration and title favicon

---
+++ src/components/stripe-button/stripe-button.component.jsx
@@ -0,0 +1,29 @@
+import StripeCheckout from "react-stripe-checkout";
+
+const StripeCheckoutButton = ({ price }) => {
+  const priceForStripe = price * 100;
+  const publishableKey =
+    "pk_test_51HM6k0Fuwc6SFexxmsOufiq9Zd6atRc7cgBDBUXUMJmUVUwSS86tBe2fktmHRb9YDgcE9xU0V3XeKcP89opvcthy00khzaxqkC";
+
+  const onToken = (token) => {
+    console.log(token);
+    alert("Payment Successful");
+  };
+
+  return (
+    <StripeCheckout
+      label="Pay Now"
+      name="SHOP Clothing Ltd."
+      billingAddress
+      shippingAddress
+      image='https://svgshare.com/i/CUz.svg'
+      description={`Your total is $${price}`}
+      amount={priceForStripe}
+      panelLabel="Pay Now"
+      token={onToken}
+      stripeKey={publishableKey}
+    />
+  );
+};
+
+export default StripeCheckoutButton;
